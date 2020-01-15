5fb36045a9fd7efce35377ca5b918858345216c1
neo <neo@T440>
2020-01-15 20:03:32 +0800

    修改

--- yntd-web/src/main/webapp/themes/admin/index/js/dashboard.js
+++ yntd-web/src/main/webapp/themes/admin/index/js/dashboard.js
@@ -41,13 +41,10 @@ app.controller('myCon', function($scope,$http,$interval) {
 			 $("#"+machineName+"_m").addClass("circle"+" "+"circle-"+status.toLowerCase()+" "+"headerBox");
 		})
 	}
-	
-	$scope.getMahineinfo=function(name){
-	   var cnc= JSON.parse(localStorage.getItem(jo[name]));
+ getMahineinfo();
+ function getMahineinfo(){
+	   var cnc= JSON.parse(localStorage.getItem("V10"));
 		$scope.cncName=cnc["machineName"];
-		$scope.cncProducts=cnc["cnc_products"];
-		$scope.cncAlivetime=cnc["cnc_alivetime"];
-		$scope.cncCycletime=cnc["cnc_cycletime"];
 		/***********机械坐标**************/
 		$scope.cncMcX=cnc["cnc_mcX"];
 		$scope.cncMcY=cnc["cnc_mcY"];
@@ -56,35 +53,120 @@ app.controller('myCon', function($scope,$http,$interval) {
 		/***********机械坐标**************/
 		/***********相对坐标**************/
 		$scope.cncRcX=cnc["cnc_rcX"];
-		$scope.cncRcY=cnc["cnc_rcX"];
-		$scope.cncRcZ=cnc["cnc_rcX"];
-		$scope.cncRcA=cnc["cnc_rcX"];
+		$scope.cncRcY=cnc["cnc_rcY"];
+		$scope.cncRcZ=cnc["cnc_rcZ"];
+		$scope.cncRcA=cnc["cnc_rcA"];
 		/***********相对坐标**************/
 		
 		/***********绝对坐标**************/
-		$scope.cncRcAbX=cnc["cnc_abX"];
-		$scope.cncRcAbY=cnc["cnc_abY"];
-		$scope.cncRcAbZ=cnc["cnc_abZ"];
-		$scope.cncRcAbA=cnc["cnc_abA"];
+		$scope.cncAbX=cnc["cnc_abX"];
+		$scope.cncAbY=cnc["cnc_abY"];
+		$scope.cncAbZ=cnc["cnc_abZ"];
+		$scope.cncAbA=cnc["cnc_abA"];
 		/***********绝对坐标**************/
+		$scope.cncMainproname=cnc["cnc_mainproname"];
+		$scope.cncSeq=cnc["cnc_seq"];
+		$scope.cncAlivetime=cnc["cnc_alivetime"];
+		$scope.cncCycletime=cnc["cnc_cycletime"];
+		$scope.cncCycletime=cnc["cnc_cycletime"];
+		$scope.cncProducts=cnc["cnc_products"];
 	}
-	
-	 initLine()
-	 function initLine(){
-		 var lineLoad=loadoption;
-		 console.info(lineLoad);
-		 var loadCharts=echarts.init($("#lineLoad").get(0));
-		 loadCharts.setOption(lineLoad,true)
-		 intiGauge();
+ 
+	 setTimeout(function(){
+		 initLoadLine()
+	 },1000)
+	 function initLoadLine(){
+		 	 var lineLoadObj=[];
+			 var loadChartsContrainer =$("div[name='loadChartsContainer']");
+			 var chartsArry=[];
+			 $.each(loadChartsContrainer,function(index,element){
+				 var k={name:"",charts:null}
+				  k.name=$(this).attr("data-type");
+				  k.charts=echarts.init(this);
+				  chartsArry.push(k);
+			 })
+			 
+			  $('#myCarousel').carousel({
+				  interval:6000,
+			  });
+			 
+			  $interval(function(){
+				  initLoadLineOption(chartsArry)
+			  },1000)
+			
 	 }
 	 
-	 function intiGauge(){
-		 var gOption=gaugeOption;
-		 var gagueCharts = $("#gagues").children("div"); 
-		 $.each(gagueCharts,function(){
-			 echarts.init(this).setOption(gOption,true);
+	 function initLoadLineOption(eArrays){
+		 var now = new Date();
+		 var time = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+		 var cnc= JSON.parse(localStorage.getItem("V10"));
+		 $.each(eArrays,function(i,em){
+			 var name=this.name;
+			 if(cnc[name]){
+				 var lineLoadOpt=loadoptions[i];
+				 var seriesData= lineLoadOpt.series[0];
+				 var xAxisData =lineLoadOpt.xAxis[0].data;
+				 xAxisData.push(time);
+				 var n = Number(cnc[name]);
+				 var i=0;
+				 if(cnc[name] && !isNaN(n)){
+					 i=parseInt(cnc[name]);
+				 }else{
+					 i=0;
+				 }
+				 seriesData.data.push(i);
+				 $scope[name]=i;
+				 seriesData.name=name;
+				 this.charts.setOption(lineLoadOpt,true);
+			 }
+			
 		 })
 	 }
+	 
+	 intiGauge();
+	 function intiGauge(){
+		 var gagueCharts = $("#gagues").children("div");
+		 var gagueArrays=[];
+		 $.each(gagueCharts,function(){
+			 var g={name:"",e:null};
+			 g.name=$(this).attr("data-type");
+			 g.e=echarts.init(this);
+			 gagueArrays.push(g);
+		 })
+		 
+		  $interval(function(){
+			   setGagueoPtion(gagueArrays);
+		  },1000)
+		 
+	 }
+	 
+	 var dataZH={
+			 cnc_srate:"主轴倍率",
+			 cnc_frate:"进给倍率",
+			 cnc_rapidfeed:"快速移动倍率"
+	 }
+	 
+	 function setGagueoPtion(eArrays){
+		 var cnc= JSON.parse(localStorage.getItem("V10"));
+		 $.each(eArrays,function(i,em){
+			 var name=this.name;
+			 var n = Number(cnc[name]);
+			 var i=0;
+			 var gOpt=gaugeOptions[i];
+			 if(cnc[name]){
+				 if(!isNaN(n)){
+					 i=parseInt(cnc[name]);
+				 }else{
+					 i=10;
+				 }
+			 }
+			 var m={name:"",value:0};
+			 m.name=dataZH[name];
+			 m.value=i;
+			 gOpt.series[0].data[0]= m;
+			 this.e.setOption(gOpt);
+		 })
+    }
 }).directive('lineCharts',function($interval,$http){
 	return{
 		restrict:'A',
--- yntd-web/src/main/webapp/themes/admin/machine/js/gaugeOption.js
+++ yntd-web/src/main/webapp/themes/admin/machine/js/gaugeOption.js
@@ -1,5 +1,4 @@
-var gaugeOption = {
-			backgroundColor:'#f5f5f5',
+var gaugeOptions = [{
 			title : {
 				top : 5,
 				left : 'center',
@@ -7,7 +6,7 @@ var gaugeOption = {
 					color : '#000'
 				}
 			},
-			toolbox :{"feature":{"saveAsImage":{"show":true}},"top":10,"left":"right"},
+			toolbox :{"feature":{"saveAsImage":{"show":false}},"top":10,"left":"right"},
 			tooltip : {
 				show : true,
 				formatter : "{c}%",
@@ -91,4 +90,185 @@ var gaugeOption = {
 				data:[{name:"asdasd",value:100}]
 				
 			}]
-};
+},{
+	title : {
+		top : 5,
+		left : 'center',
+		textStyle : {
+			color : '#000'
+		}
+	},
+	toolbox :{"feature":{"saveAsImage":{"show":false}},"top":10,"left":"right"},
+	tooltip : {
+		show : true,
+		formatter : "{c}%",
+		backgroundColor : '#F7F9FB',
+		borderColor : '#92DAFF',
+		borderWidth : '1px',
+		textStyle : {
+			color : 'black'
+		}
+	},
+	series : [{
+		name : '达标率',
+		type : 'gauge',
+		max : 150,
+		radius : '75%', //图表尺寸
+		center : [ "50%", "45%" ],
+		axisLine : {
+			show : true,
+			lineStyle : {
+				width : 5,
+				shadowColor : 'rgba(0,0,0,0.4)',
+				shadowOffsetY : 10,
+				shadowBlur : 10,
+				color : [ [ 0.3, '#DD3F36' ], [ 0.7, '#FAE521' ],
+						[ 1, '#37B70C' ] ]
+			}
+		},
+		axisTick : {
+			show : false,
+			splitNumber : 10
+		},
+		splitLine : {
+			show : true,
+			length : 10,
+			lineStyle : {
+				color : 'rgba(0,0,0,0.6)'
+			}
+		},
+		axisLabel : {
+			distance : 5,
+			textStyle : {
+				color : "#000",
+				fontSize : 10,
+			},
+			formatter : function(e) {
+				return e
+			}
+		},
+		pointer : {
+			show : true,
+			length : '80%',
+			width : 2
+		},
+		detail : {
+			show : true,
+			offsetCenter : [ 0, '80%' ],
+			formatter:"{value}%",
+			width : 56,
+			height : 20,
+			textStyle : {
+				fontSize : 13,
+				color : "black",
+				fontWeight : 'bolder'
+			},
+			backgroundColor : '#A9DAFF',
+			borderWidth : 2,
+			borderColor : '#45E4D0',
+			shadowColor : 'black', //默认透明
+			shadowBlur : 5
+
+		},
+		title : {
+			textStyle : {
+				fontSize : 16,
+				fontWeight : 'bolder',
+				fontStyle : 'normal',
+				color : "black"
+			},
+			offsetCenter : [ 0, '30%' ]
+		},
+		data:[{name:"asdasd",value:100}]
+		
+	}]
+},{
+	title : {
+		top : 5,
+		left : 'center',
+		textStyle : {
+			color : '#000'
+		}
+	},
+	toolbox :{"feature":{"saveAsImage":{"show":false}},"top":10,"left":"right"},
+	tooltip : {
+		show : true,
+		formatter : "{c}%",
+		backgroundColor : '#F7F9FB',
+		borderColor : '#92DAFF',
+		borderWidth : '1px',
+		textStyle : {
+			color : 'black'
+		}
+	},
+	series : [{
+		name : '达标率',
+		type : 'gauge',
+		max : 150,
+		radius : '75%', //图表尺寸
+		center : [ "50%", "45%" ],
+		axisLine : {
+			show : true,
+			lineStyle : {
+				width : 5,
+				shadowColor : 'rgba(0,0,0,0.4)',
+				shadowOffsetY : 10,
+				shadowBlur : 10,
+				color : [ [ 0.3, '#DD3F36' ], [ 0.7, '#FAE521' ],
+						[ 1, '#37B70C' ] ]
+			}
+		},
+		axisTick : {
+			show : false,
+			splitNumber : 10
+		},
+		splitLine : {
+			show : true,
+			length : 10,
+			lineStyle : {
+				color : 'rgba(0,0,0,0.6)'
+			}
+		},
+		axisLabel : {
+			distance : 5,
+			textStyle : {
+				color : "#000",
+				fontSize : 10,
+			},
+			formatter : function(e) {
+				return e
+			}
+		},
+		pointer : {
+			show : true,
+			length : '80%',
+			width : 2
+		},
+		detail : {
+			show : true,
+			offsetCenter : [ 0, '80%' ],
+			formatter:"{value}%",
+			width : 56,
+			height : 20,
+			textStyle : {
+				fontSize : 13,
+				color : "black",
+				fontWeight : 'bolder'
+			},
+			backgroundColor : '#A9DAFF',
+			borderWidth : 2,
+			borderColor : '#45E4D0',
+			shadowColor : 'black', //默认透明
+			shadowBlur : 5
+		},
+		title : {
+			textStyle : {
+				fontSize : 10,
+				color : "black"
+			},
+			offsetCenter : [ 0, '30%' ]
+		},
+		data:[{name:"asdasd",value:100}]
+		
+	}]
+}]
